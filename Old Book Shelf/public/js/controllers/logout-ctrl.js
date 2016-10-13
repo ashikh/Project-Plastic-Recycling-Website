34c866d1e0ca34b8ae2f0b4026470791ee173e6a
ashikh <ashee.vajra@gmail.com>
2016-10-13 12:05:57 +0530

    Carton-Plastic Recycling Service Website

--- Old Book Shelf/public/js/controllers/logout-ctrl.js
+++
@@ -1,9 +0,0 @@
-angular.module("OldBookShelf")
-
-.controller("logoutCtrl",function($rootScope,$location){
-    
-    console.log("Logout Controller ..")
-    $rootScope.isAuthenticated = false;
-    $location.url('/login');
-    
-});
